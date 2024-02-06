d43ae1f976b5427c528d994e65362e0ab940dec5
Mian Furqan Arshad <152207102+MainFurqan@users.noreply.github.com>
2024-02-06 09:57:53 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ from langchain.schema import (
 )
 import json
 
-openapi_key = st.secrets["OPENAI_API_KEY"]
+openapi_key = "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"
 
 # Set streamlit page configuration
 st.set_page_config(page_title="MFA Project_1")
